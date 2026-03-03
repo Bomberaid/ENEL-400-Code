93a5bbe12e523a57e5d1224ecbacc27ad6f676c2
Alvin <alvin1312@icloud.com>
2026-03-03 00:17:51 -0700

    Added motor, servo, and wireless server functionality

---
+++ esp32_server_communication/esp32_server_communication.ino
@@ -0,0 +1,64 @@
+#include <WiFi.h>
+#include <HTTPClient.h>
+//#include <ArduinoJson.h>
+
+const char* ssid = "alvinkhuu";
+const char* password = "AlvinKhuu";
+
+// Your Flask server endpoint
+const char* SERVER_URL = "http://209.38.135.76/movement";
+
+//uint32_t counter = 0;
+const int joystickPin = 32;
+const int servoPin = 33;
+
+void setup() {
+  Serial.begin(115200);
+  delay(500);
+
+  pinMode(joystickPin, INPUT);
+  pinMode(servoPin, INPUT);
+
+  WiFi.mode(WIFI_STA);
+  WiFi.setSleep(false);
+  WiFi.begin(ssid, password);
+
+  Serial.print("Connecting");
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(250);
+    Serial.print(".");
+  }
+
+  Serial.println("\nConnected!");
+  Serial.print("ESP32 IP: ");
+  Serial.println(WiFi.localIP());
+}
+
+void loop() {
+
+  if (WiFi.status() == WL_CONNECTED) {
+
+    int joystickValue = analogRead(joystickPin);
+    int servoValue = analogRead(servoPin);
+
+    HTTPClient http;
+    http.begin(SERVER_URL);
+    http.addHeader("Content-Type", "application/json");
+
+    // Create JSON body
+    String json = "{";
+    json += "\"left/right\": " + String(servoValue) + ",";
+    json += "\"up/down\": " + String(joystickValue);
+    json += "}";
+
+    int httpResponseCode = http.POST(json);
+
+    Serial.println(json);
+    Serial.print("Response code: ");
+    Serial.println(httpResponseCode);
+
+    http.end();
+  }
+
+  delay(100);
+}
--- reciever/reciever.ino
+++ reciever/reciever.ino
@@ -1,24 +1,55 @@
 #include <WiFi.h>
-#include <WiFiUdp.h>
-#include <string>
+#include <HTTPClient.h>
+#include <ArduinoJson.h>
+#include <ESP32Servo.h>
 
-const char* ssid = "Fi";
-const char* password = "gN5ehgN%";
+const char* ssid = "alvinkhuu";
+const char* password = "AlvinKhuu";
 
-WiFiUDP udp;
-const uint16_t LISTEN_PORT = 5000;
+const char* SERVER_URL = "http://209.38.135.76/movement";
 
-char buf[512];
-const int motorPin = 32;
+const int forwardMotorPin = 25;
+const int reverseMotorPin = 26;
+const int forwardEnablePin = 12;
+const int reverseEnablePin = 14;
+const int servoPin = 18;
+const int motorDutyCycleLimit = 100;
+
+// Servo Variables
+Servo steeringServo;
+const int xMin = 0;
+const int xCenter = 1940;
+const int xMax = 4095;
+
+const int deadZone = 120;
+const int centerLockZone = 180;
+
+const int leftAngle = 70;
+const int centerAngle = 90;
+const int rightAngle = 110;
+
+float filteredX = xCenter;
+
+int lastServoAngle = centerAngle;
+
+unsigned long lastUpdate = 0;
+const int updateInterval = 5;
 
 void setup() {
   Serial.begin(115200);
   delay(500);
 
-  pinMode(motorPin, OUTPUT);
+  pinMode(forwardMotorPin, OUTPUT);
+  pinMode(reverseMotorPin, OUTPUT);
+  pinMode(forwardEnablePin, OUTPUT);
+  pinMode(reverseEnablePin, OUTPUT);
+
+  steeringServo.setPeriodHertz(50);
+  steeringServo.attach(servoPin, 500, 2500);
+  steeringServo.write(centerAngle);
 
   WiFi.mode(WIFI_STA);
-  WiFi.setSleep(false);   // helps reduce lag/jitter
+  WiFi.setSleep(false);
   WiFi.begin(ssid, password);
 
   Serial.print("Connecting");
@@ -26,36 +57,108 @@ void setup() {
     delay(250);
     Serial.print(".");
   }
-  Serial.println("\nConnected!");
-  Serial.print("Receiver IP: ");
-  Serial.println(WiFi.localIP());
 
-  udp.begin(LISTEN_PORT);
-  Serial.println("Listening on UDP port 5000");
+  Serial.println("\nConnected!");
+  Serial.print("ESP32 IP: ");
+  Serial.println(WiFi.localIP());
+}
+
+void servoCode(int raw) {
+  filteredX = 0.70 * filteredX + 0.30 * raw;
+  int xSmooth = (int)filteredX;
+
+  int error = xSmooth - xCenter;
+  int servoAngle = centerAngle;
+
+  if (abs(error) <= centerLockZone) {
+    servoAngle = centerAngle;
+  }
+  else if (xSmooth < xCenter - deadZone) {
+    servoAngle = map(xSmooth, xMin, xCenter - deadZone, rightAngle, centerAngle);
+    servoAngle = constrain(servoAngle, centerAngle, rightAngle);
+  }
+  else if (xSmooth > xCenter + deadZone) {
+    servoAngle = map(xSmooth, xCenter + deadZone, xMax, centerAngle, leftAngle);
+    servoAngle = constrain(servoAngle, leftAngle, centerAngle);
+  }
+
+  if (millis() - lastUpdate >= updateInterval) {
+    if (abs(servoAngle - lastServoAngle) >= 1) {
+      steeringServo.write(servoAngle);
+      lastServoAngle = servoAngle;
+    }
+    lastUpdate = millis();
+  }
+
+  Serial.print("Servo angle: ");
+  Serial.println(servoAngle);
 }
 
 void loop() {
-  int packetSize = udp.parsePacket();
-  if (!packetSize) return;
 
-  int len = udp.read(buf, sizeof(buf) - 1);
-  if (len <= 0) return;
-  buf[len] = '\0';
+  if (WiFi.status() == WL_CONNECTED) {
 
-  // Serial.print("From ");
-  // Serial.print(udp.remoteIP());
-  // Serial.print(": ");
-  // Serial.println(buf);
+    HTTPClient http;
+    http.begin(SERVER_URL);
 
-  int thumbstick_value = std::stoi(buf);
-  int forward_value = map(thumbstick_value, 1900, 4095, 0, 255);
-  int backward_value = map(thumbstick_value, 0, 2000, 255, 0);
-  Serial.println(thumbstick_value);
+    int httpCode = http.GET();
 
-  analogWrite(motorPin, forward_value);
-  // if (thumbstick_value > 3000) {
-  //   digitalWrite(motorPin, HIGH);
-  //   Serial.println("running");
-  // } 
-  // else digitalWrite(motorPin, LOW);
+    if (httpCode == HTTP_CODE_OK) {
+
+      String payload = http.getString();
+      Serial.println(payload);
+
+      // Parse JSON
+      StaticJsonDocument<200> doc;
+      DeserializationError err = deserializeJson(doc, payload);
+
+      if (!err) {
+        int motor_value = doc["up/down"];
+        int servo_value = doc["left/right"];
+        int center = 1950;
+        int deadzone = 100;
+
+        int duty = 0;
+
+        if (motor_value > center + deadzone) {
+            // Set direction forward
+            duty = map(motor_value, center, 4095, 0, motorDutyCycleLimit);
+            analogWrite(forwardMotorPin, duty);
+
+            // Both enable pins must be on to enable PWM mode on motor driver.
+            digitalWrite(forwardEnablePin, HIGH);
+            digitalWrite(reverseEnablePin, HIGH);
+        }
+        else if (motor_value < center - deadzone) {
+            duty = map(motor_value, 0, center, motorDutyCycleLimit, 0);
+            analogWrite(reverseMotorPin, duty);
+
+            digitalWrite(forwardEnablePin, HIGH);
+            digitalWrite(reverseEnablePin, HIGH);
+        }
+        else {
+            duty = 0;  // stop
+            analogWrite(forwardMotorPin, duty);
+            analogWrite(reverseMotorPin, duty);
+
+            digitalWrite(forwardEnablePin, LOW);
+            digitalWrite(reverseEnablePin, LOW);
+        }
+        Serial.println(duty);
+
+        servoCode(servo_value);
+      }
+      else {
+        Serial.println("JSON parse failed");
+      }
+    }
+    else {
+      Serial.print("HTTP error: ");
+      Serial.println(httpCode);
+    }
+
+    http.end();
+  }
+
+  delay(100);   // polling rate
 }
