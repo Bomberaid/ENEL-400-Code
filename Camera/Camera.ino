80bd8b9b5cb5f9b7c7bbae3cbe36c7c199519810
Bomberaid <alvin1312@icloud.com>
2026-03-31 22:21:10 -0600

    fixed frame drop problem

--- Camera/Camera.ino
+++ Camera/Camera.ino
@@ -61,7 +61,7 @@ void startCamera() {
   config.pin_sccb_scl = SIOC_GPIO_NUM;
   config.pin_pwdn     = PWDN_GPIO_NUM;
   config.pin_reset    = RESET_GPIO_NUM;
-  config.xclk_freq_hz = 20000000;
+  config.xclk_freq_hz = 10000000;
   config.pixel_format = PIXFORMAT_JPEG;
   config.grab_mode    = CAMERA_GRAB_LATEST; // Always grab newest frame
   config.jpeg_quality = 12;
